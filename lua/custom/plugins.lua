9a50bddf0f1072727f28d078278202b7690018b5
IIIzP0III <zP@solarsystem.coffee>
2023-09-14 18:04:47 -0800

    init

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,29 @@
+local plugins={
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function ()
+      require("chatgpt").setup({
+        api_key_cmd = "pass show jippity-nvim-api-key",
+      })    
+    end,
+  
+  },
+  {
+    "natebosch/vim-lsc",
+  }
+
+}
+return plugins
